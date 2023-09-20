a021d348981f963705cedf3db245943fb05c6cad
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-09-20 19:21:57 -0300

    documenta o código

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -1,19 +1,28 @@
 import Race from './Race';
 
+// ao extender de Race, eu herdo suas características
 export default class Dwarf extends Race {
   private _maxLifePoints: number;
-  private static raceInstance = 0;
+  private static raceInstance = 0; // quantidade de anões criados, iniciando em zero
 
+  // inicio meus atributos no construtor,
+  // herdo name e destreza de Race
+  // aponto para isso com o super
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 80;
-    Dwarf.raceInstance += 1;
+    Dwarf.raceInstance += 1; // sempre que eu instanciar um novo objeto anão, incremento aqui
   }
 
+  // implementação do método abstrato que herdei
   get maxLifePoints(): number {
     return this._maxLifePoints;
   }
 
+  // implementação do método statico que herdei,
+  // ademais, uso o override para sobrescrever o método
+  // que agora não retorna um erro, mas sim quantos anões
+  // foram criados
   static override createdRacesInstances(): number {
     return Dwarf.raceInstance;
   }
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -2,11 +2,13 @@ export default abstract class Race {
   private _name: string;
   private _dexterity: number;
 
+  // constructor básico, iniciando meus atributos
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
   }
 
+  // métodos getter, para visualização
   get name(): string {
     return this._name;
   }
@@ -14,10 +16,22 @@ export default abstract class Race {
   get dexterity(): number {
     return this._dexterity;
   }
-
+  
+  // método statico retorna sempre um mesmo valor,
+  // o throwing é uma garantia de que terei que 
+  // implementar o retorno do método nas class filhas
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
 
+  // método abstrato, ele será implementado de fato
+  // nas classes que forem herdar Race
+  // aqui ele só é declarado
   abstract get maxLifePoints(): number;
 }
+
+// lembrando a fórmula clássica de uma classe
+// estruturada da seguinte forma:
+// atributos
+// construtor
+// e métodos

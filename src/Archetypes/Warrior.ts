78bd57745d5f13013d33540a8cf5cd9ba3cb3b17
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-09-21 09:44:58 -0300

    feat: create class archetypes

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static classInstace = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior.classInstace += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static override createdArchetypeInstances(): number {
+    return Warrior.classInstace;
+  }
+}

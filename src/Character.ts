9ce4a33c31e608699c4c920cefe45c77cb81f43d
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-09-22 09:58:12 -0300

    req 7 full

--- src/Character.ts
+++ src/Character.ts
@@ -7,27 +7,100 @@ import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private name: string;
-  private race: Race;
-  private archetype: Archetype;
+  private _race: Race;
+  private _archetype: Archetype;
   private maxLifePoints: number;
-  private lifePoints: number;
-  private strength: number;
-  private defense: number;
-  private dexterity: number;
-  private energy: Energy;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
 
   constructor(name: string) {
     this.name = name;
-    this.dexterity = getRandomInt(1, 10);
-    this.race = new Elf(name, this.dexterity);
-    this.archetype = new Mage(name);
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this.dexterity);
+    this._archetype = new Mage(name);
     this.maxLifePoints = this.race.maxLifePoints / 2;
-    this.lifePoints = this.maxLifePoints;
-    this.strength = getRandomInt(1, 10);
-    this.defense = getRandomInt(1, 10);
-    this.energy = {
+    this._lifePoints = this.maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
       type_: this.archetype.energyType,
       amount: getRandomInt(1, 10),
     };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const dmg = attackPoints - this._defense;
+
+    if (dmg > 0) {
+      this._lifePoints -= dmg;
+    } else {
+      this._lifePoints -= 1;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+  
+  levelUp(): void {
+    this.maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+
+    if (this.maxLifePoints > this.race.maxLifePoints) {
+      this.maxLifePoints = this.race.maxLifePoints;
+    }
+    
+    this._lifePoints = this.maxLifePoints;
+  }
+
+  // dedo da morte de Vecna hehe
+  // se meu inimigo tem pontos de vida menores do que o meu
+  // e tem menos de 100 pontos de vida
+  // eu posso executá-lo com meu feitiço
+
+  special?(enemy: Fighter): void {
+    if (enemy.lifePoints < this._lifePoints && enemy.lifePoints < 100) {
+      enemy.receiveDamage(enemy.lifePoints);
+    }
   }
 }
